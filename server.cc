92b3e41921bc220615724e6cb16230d710a6bb47
Moidhas <moidk3026@gmail.com>
2025-01-10 18:48:48 -0500

    Refactored event loop

--- server.cc
+++ server.cc
@@ -1,18 +1,11 @@
-#include <cstdlib>
 #include <unistd.h>
-#include <sys/socket.h> 
 #include <iostream>
-#include <cstdint>
-#include <netinet/in.h>
-#include <sys/_types/_socklen_t.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <sys/event.h>
-#include <arpa/inet.h>
 #include <unordered_map>
 #include <fcntl.h>
-#include <cstring>
 #include <errno.h>
+#include <cstring>
 
 #define PORT 49152
 #define BACKLOG 5
@@ -25,20 +18,30 @@ static void die(const char *msg) {
 struct Conn {
     int fd = -1;
     uint32_t len = 0;
+    uint32_t posn = 0;
     char *buffer = nullptr;
 
     Conn(int fd): fd{fd} {}
     Conn() {}
 
-    void setBuffer(char *buff) {
-        buffer = buff;
+    void newBuffer(uint32_t length) {
+        delete[] start;
+
+        len = length;
+        posn = 0;
+
+        buffer = new char[len + 1];
+        buffer[len] = '\0';
         start = buffer;
     }
 
     void clearBuffer() {
         delete []start;
-        setBuffer(nullptr);
+
         len = 0;
+        posn = 0;
+        buffer = nullptr;
+        start = buffer;
     }
 
     ~Conn() {
@@ -57,19 +60,15 @@ int handleRead(Conn &conn, int bytes) {
         } else {
             rbytes = recv(conn.fd, &conn.len, sizeof(conn.len), 0);
             if (rbytes < 0) return rbytes;
-            conn.len = ntohl(conn.len);
-            conn.setBuffer(new char[conn.len]);
+            conn.newBuffer(ntohl(conn.len));
         }
     } 
 
     if (bytes - rbytes !=  0) {
-        int rbyte = recv(conn.fd, conn.buffer, conn.len, 0);
+        int rbyte = recv(conn.fd, conn.buffer, conn.len - conn.posn, 0);
         if (rbyte < 0) return rbyte;
 
-        if (rbyte == conn.len) {
-            std::cout << "msg: " << conn.buffer << std::endl;
-            conn.clearBuffer();
-        }
+        conn.posn += rbyte;
 
         rbytes += rbyte;
     }
@@ -77,8 +76,14 @@ int handleRead(Conn &conn, int bytes) {
     return rbytes;
 }
 
-int main() {
-    int ret;
+// Accepts the connection file-descriptor.
+int acceptConn(int fd, int kq) {
+    int connfd = accept(fd, NULL, NULL);
+    if (connfd >= 0) fcntl(connfd, F_SETFL, O_NONBLOCK); 
+    return connfd; 
+}
+
+int setupListenSocket() {
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
@@ -86,26 +91,34 @@ int main() {
     memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
 
     int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    fcntl(listenfd, F_SETFL, O_NONBLOCK);
 
-    if (bind(listenfd, (sockaddr *) &addr, sizeof(addr)) < 0) {
-        die("bind()");
-    }
+    int ret = bind(listenfd, (sockaddr *) &addr, sizeof(addr));
+    if (ret < 0) return ret;
     
     listen(listenfd, BACKLOG);
-    char ipstr[INET_ADDRSTRLEN];
-
-    int kq = kqueue();
-    if (kq < 0) return - 1; 
+    return listenfd; 
+}
 
 
+
+int main() {
+    int ret;
     std::unordered_map<int, Conn> conns;
+
+    int listenfd = setupListenSocket();
+    if (listenfd < 0) die("setupListenSocket()");
+
+    
+    int kq = kqueue();
+    if (kq < 0) die("kevent()");
     struct kevent evSet; 
     struct kevent tevent;
-
     EV_SET(&evSet, listenfd, EVFILT_READ, EV_ADD, 0, BACKLOG, NULL);
     ret = kevent(kq, &evSet, 1, NULL, 0, NULL);
     if (ret < 0) die("kevent()");
 
+    // event loop.
     while (true) {
         ret = kevent(kq, NULL, 0, &tevent, 1, NULL);
         if (ret <= 0) {
@@ -120,17 +133,24 @@ int main() {
             }
 
             if (tevent.ident == listenfd && tevent.filter == EVFILT_READ) {
-                int connfd = accept(listenfd, NULL, NULL);
+                int connfd = acceptConn(listenfd, kq);
                 if (connfd < 0) die("accept()");
-                fcntl(connfd, F_SETFL, O_NONBLOCK);
-                conns[connfd] = Conn{connfd};
 
                 EV_SET(&evSet, connfd, EVFILT_READ, EV_ADD, 0, 0, NULL);
                 ret = kevent(kq, &evSet, 1, NULL, 0, NULL);
                 if (ret < 0) die("kevent()");
+
+                conns[connfd] = Conn{connfd};
             } else if (tevent.filter == EVFILT_READ) {
-                int rbytes = handleRead(conns[tevent.ident], tevent.data);
+                Conn &conn = conns[tevent.ident];
+
+                int rbytes = handleRead(conn, tevent.data);
                 if (rbytes < 0)  die("handleRead()");
+
+                if (conn.posn  == conn.len) {
+                    std::cout << "msg: " << conn.buffer << std::endl;
+                    conn.clearBuffer();
+                }
             }
         }
 
